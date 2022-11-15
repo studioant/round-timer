f53cd11fa7660b3368f33ea0054bf15c74d0211b
studioant <weareallone83@gmail.com>
2022-11-16 07:31:22 +1100

    edit Clock component

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,7 +1,7 @@
 const Clock = ({ secs, ms }) => {
   return (
     <div className="clock">
-        {secs > 10 && <div className="timer">0{secs}.</div> }
+        {secs > 10 && <div className="timer">{secs}.</div> }
         <div className="timer">{ms}</div>
     </div>
   )
