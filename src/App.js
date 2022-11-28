2c6a7446e70344b687d0f42feba47785e5c7fb30
studioant <weareallone83@gmail.com>
2022-11-29 08:43:48 +1100

    fix sizing consistency

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [round, setRound] = useState(0)
   const [time, setTime] = useState({ms:0, s:3, m:0})
   const [started, setStarted] = useState(false)
+  const [zero, setZero] = useState(false)
   const [inter, setInter] = useState(1)
 
   useEffect(()=>{
@@ -30,6 +31,7 @@ function App() {
 
   const stop = () => {
     setStarted(!started)
+    setZero(true)
   }
 
   const reset = () => {
@@ -82,18 +84,21 @@ function App() {
         <Header title={'Round'}/>
         <Round round={round}/>
         <div className="btn-round">
-          <Button color='#567' text='Decrement' onClick={()=>setRound(round => round - 1)}/>
+          <Button color='#567' text='Decrement' onClick={()=> {round > 0 && (setRound(round => round - 1))}}/>
           <Button color='#345' text='Increment' onClick={()=>setRound(round => round + 1)}/>
+          <Button color='#804040' text='Reset' onClick={()=>setRound(0)}/>
         </div>
       </div>
       <div className="container-clock">
         <Header title={'Countdown Timer'}/>
-        <Clock min={time.m} sec={time.s} ms={time.ms}/>
+        <div className="inner-clock">
+          <Clock min={time.m} sec={time.s} ms={time.ms}/>
+        </div>
         <div className="btn-clock">
           <Button color={started ? '#ca3433' :'#06a94d'} text={started ? 'Stop' : 'Start'} onClick={started ? stop : start}/>
-          <Button color='#567' text='Reset' onClick={reset} state={started}/>
-          <Button color='#567' text='+1 sec' onClick={inc}/>
-          <Button color='#567' text='-1 sec' onClick={dec}/>
+          <Button color='#804040' text='Reset' onClick={reset} state={started}/>
+          <Button color='#567' text='+1 sec' onClick={()=>{!started && (inc())}}/>
+          <Button color='#567' text='-1 sec' onClick={()=>{!started && (dec())}}/>
           <Button color='#567' text='Play' onClick={()=>playAlarm()}/>
         </div>
       </div>
